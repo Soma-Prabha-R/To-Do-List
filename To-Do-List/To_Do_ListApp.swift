73821d8b3abdc729b9de2f0ac97bb6ff2269ce29
Soma-Prabha-R <Somaprabha61@gmail.com>
2024-08-12 15:52:49 +0530

    Initial Commit

---
+++ To-Do-List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To-Do-List
+//
+//  Created by Soma Prabha R on 12/08/24.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
